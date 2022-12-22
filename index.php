f1e4c4df362199fbd756fde5785437f6839d7071
alessandrolega <alessandrolega24@gmail.com>
2022-12-22 01:14:58 +0100

    istanziata class dvd

--- index.php
+++ index.php
@@ -4,19 +4,22 @@ include_once __DIR__ . '/classi/generi.php';
 include_once __DIR__ . '/classi/libro.php';
 include_once __DIR__ . '/classi/audiolibro.php';
 include_once __DIR__ . '/classi/cd.php';
+include_once __DIR__ . '/classi/dvd.php';
 
 
 $generi = [
   'giallo' => new Generi('giallo', 'icon-font-awesome-giallo'),
   'horror' => new Generi('horror', 'icon-font-awesome-horror'),
-  'fantasy' => new Generi('fantasy', 'icon-font-awesome-fantasy')
+  'fantasy' => new Generi('fantasy', 'icon-font-awesome-fantasy'),
+  'azione' => new Generi('azione', 'icon-font-awesome-fantasy')
 ];
 var_dump($generi);
 
 $prodotti = [
   new Libro('La scelta di Natan', 'Antonio Puccio', 23.40, $generi['giallo'], true, 'https://immagine-copertina.com', 250, 'flessibile'),
   new audioLibro('La scelta di Natan', 'Antonio Puccio', 23.40, $generi['giallo'], true, 'https://immagine-copertina.com', 120, 'spotify', 'https://spotify-libri.com'),
-  new Cd('Il signore degli anelli', 'JR Tolkien', 25.40, $generi['fantasy'], true, 'https://immagine-copertina.com', 140, 1954)
+  new Cd('Il signore degli anelli', 'JR Tolkien', 25.40, $generi['fantasy'], true, 'https://immagine-copertina.com', 140, 1954),
+  new Dvd('Fast and Furious 10', 'Universal', 19.99, $generi['azione'], false, 'https://immagine-copertina.com', 130, 2025)
 ];
 
 var_dump($prodotti);
