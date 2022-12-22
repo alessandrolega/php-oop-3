a8a85e27407e7d2d376d93f9d020f7fa5be734cf
alessandrolega <alessandrolega24@gmail.com>
2022-12-22 01:04:48 +0100

    istanziata class cd

--- index.php
+++ index.php
@@ -3,22 +3,25 @@
 include_once __DIR__ . '/classi/generi.php';
 include_once __DIR__ . '/classi/libro.php';
 include_once __DIR__ . '/classi/audiolibro.php';
+include_once __DIR__ . '/classi/cd.php';
 
 
 $generi = [
   'giallo' => new Generi('giallo', 'icon-font-awesome-giallo'),
-  'horror' => new Generi('horror', 'icon-font-awesome-horror')
+  'horror' => new Generi('horror', 'icon-font-awesome-horror'),
+  'fantasy' => new Generi('fantasy', 'icon-font-awesome-fantasy')
 ];
-var_dump( $generi );
+var_dump($generi);
 
 $prodotti = [
-  new Libro( 'La scelta di Natan', 'Antonio Puccio', 23.40, $generi['giallo'], true, 'https://immagine-copertina.com', 250, 'flessibile' ),
+  new Libro('La scelta di Natan', 'Antonio Puccio', 23.40, $generi['giallo'], true, 'https://immagine-copertina.com', 250, 'flessibile'),
   new audioLibro('La scelta di Natan', 'Antonio Puccio', 23.40, $generi['giallo'], true, 'https://immagine-copertina.com', 120, 'spotify', 'https://spotify-libri.com'),
+  new Cd('Il signore degli anelli', 'JR Tolkien', 25.40, $generi['fantasy'], true, 'https://immagine-copertina.com', 140, 1954)
 ];
 
-var_dump( $prodotti );
+var_dump($prodotti);
 
-foreach( $prodotti as $elem ){
+foreach ($prodotti as $elem) {
   echo get_class($elem);
   echo "<br>";
 }
